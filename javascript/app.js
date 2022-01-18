011a119b92152d755f85a2871541fd26a9a947ee
peter.chen1024 <u0052017@gmail.com>
2022-01-18 17:38:16 +0800

    登入頁

---
+++ javascript/app.js
@@ -0,0 +1,29 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.27/vue.esm-browser.min.js';
+
+
+const app = createApp({
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    login() {
+      const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+      axios.post(api, this.user).then((res) => {
+        const { token, expired } = res.data;
+        // 寫入 cookie token
+        // expires 設置有效時間
+        document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
+        window.location = 'product.html';
+      }).catch((err) => {
+        console.log(err.data.message);
+      })
+    },
+  }
+});
+
+app.mount('#app');
