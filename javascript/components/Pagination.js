fa9e982fa74d24159459c3a01f2d4d4ccdcc8524
peter.chen1024 <u0052017@gmail.com>
2022-02-15 10:39:26 +0800

    add click.prevent

--- javascript/components/Pagination.js
+++ javascript/components/Pagination.js
@@ -4,16 +4,16 @@ export default {
   <ul class="pagination justify-content-center">
     <li class="page-item" :class="{ disabled : !pagination.has_pre }">
       <a class="page-link" href="#" aria-label="Previous"
-      @click="$emit('get-products', pagination.current_page - 1)">
+      @click.prevent="$emit('get-products', pagination.current_page - 1)">
         <span aria-hidden="true">&laquo;</span>
       </a>
     </li>
     <li class="page-item" v-for="page in pagination.total_pages" :key="'page'+page"
     :class="{ active : page === pagination.current_page }">
-      <a class="page-link" href="#" @click="$emit('get-products', page)">{{page}}</a></li>
+      <a class="page-link" href="#" @click.prevent="$emit('get-products', page)">{{page}}</a></li>
     <li class="page-item" :class="{ disabled : !pagination.has_next }">
       <a class="page-link" href="#" aria-label="Next"
-      @click="$emit('get-products', pagination.current_page + 1)">
+      @click.prevent="$emit('get-products', pagination.current_page + 1)">
         <span aria-hidden="true">&raquo;</span>
       </a>
     </li>
